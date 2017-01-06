5189b1b458b94c451c6ecffa4d535ec56d4920a8
mboussaa <mohamed.boussaa@inria.fr>
2017-01-06 09:21:29 +0100

    First commit

--- src/main/java/ArchGroup/ArchID/App.java
+++ src/main/java/ArchGroup/ArchID/App.java
@@ -8,6 +8,11 @@ public class App
 {
     public static void main( String[] args )
     {
+    	String cd="rvfv";
+    	String cd1="rvfv";
+    	String cd22="rvfv";
+    	String cd33="rvfv";
+    	String cd44="rvfv";
     	  System.out.println( "************************************************************************************************************************************************************************" );
         System.out.println( "Hello World!" );
     }
