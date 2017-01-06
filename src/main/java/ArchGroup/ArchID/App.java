e4badd5d1a0861d9d5654cdf0bf53bc75262ab17
mboussaa <mohamed.boussaa@inria.fr>
2017-01-06 08:13:58 +0100

    First commit

---
+++ src/main/java/ArchGroup/ArchID/App.java
@@ -0,0 +1,13 @@
+package ArchGroup.ArchID;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
